443d9250785ab014098cc11ef2bfa64c49d21cbc
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-07-06 01:14:30 +0530

    login Ui and function

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'] // Corrected to `styleUrls`
 })
 export class LoginComponent {
 
+  loginObj: any = {
+    "EmailId": "",
+    "Password": ""
+  }
+  userService = inject(UserService);
+  router = inject(Router);
+
+  login() {
+    this.userService.onLogin(this.loginObj).subscribe((res: any) => {
+      if (res.result) {
+        localStorage.setItem("userApp", JSON.stringify(res.data));
+        this.router.navigateByUrl("user-list");
+      } else {
+        alert(res.message);
+      }
+    })
+  }
 }
